34188736a72a8caf028c4592d5ad4ae20dacdc41
Phoenicis <alliefirebaugh@gmail.com>
2018-12-01 17:19:25 -0500

    starting to add sound effects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,17 +12,33 @@ $(document).ready(function() {
     // console.log(currentScore.reduce(reducer));
     // Two lines above were (old) attempts for trying to sum up an array to keep the currentScore counting on the page. I found a much better way, but I wanted to keep this for future reference since I spent time learning about the reduce function.
 
+    // Adding in sound effects as a bonus:
+    var crystalClick = document.createElement("audio");
+    crystalClick.setAttribute("src", "../sounds/gem.mp3");
+
+    $('#magicImageGem').on('click', function() {
+        crystalClick.play();
+    });
+
+    var winSound = document.createElement("audio");
+    winSound.setAttribute("src", "../sounds/win.mp3");
+
+    var lossSound = document.createElement("audio");
+    lossSound.setAttribute("src", "../sounds/loss.mp3");
+    
+
     // reset the score on a page load
     $("#wins").text("Wins = " + wins);
     $("#losses").text("Losses = " + losses);
 
     function start() {
+        currentScore = 0;
         $("#currentScore").text("Current Score is: " + currentScore);
 
         // Generate the random target number, kept separate from the gem function because I wanted to pass through 19-120 to make sure to get this entire interval in my random number generator here
         function goldenNumber(min, max) {
             ranNum = Math.floor(Math.random() * (max - min)) + min;
-            $("#ranNumBox").append(ranNum);
+            $("#ranNumBox").text(ranNum);
         } // End goldenNumber
 
         // Generate random values for each gem (variable)
@@ -34,7 +50,7 @@ $(document).ready(function() {
         crystalArray = [];
         for (let i = 0; i < 4; i++) {
             crystalArray.push(resetGem());
-            console.log(crystalArray);
+            console.log(crystalArray[i]); // Shows you the randomly generated gem values in the console if you feel like cheating the game, lol.
         }
         goldenNumber(19, 120);
         resetGem();
@@ -62,17 +78,12 @@ $(document).ready(function() {
         $("#currentScore").text("Current score is: " + currentScore);
         if (currentScore > ranNum) {
             losses++;
+            start();
             $("#losses").text("Losses: " + losses);
-            console.log(currentScore);
-            console.log(crystalArray);
-            console.log(losses);
         } else if (currentScore === ranNum) {
-            
             wins++;
             $("#wins").text("Wins: " + wins);
-            console.log(currentScore);
-            console.log(crystalArray);
-            console.log(wins);
+            start();
         }
     } // End gamePlay
 }); // End document.ready
